460cb4852dddfa0e4519f693f90b24229b6e0183
Anastasiia Yerashova <yerashova.a@gmail.com>
2024-09-22 16:44:08 +0200

    done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,79 +1,13 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+export const BASE_URL = "https://pixabay.com/api/";
+export const API_KEY = "46106680-1f6bf9cc8c5ebc2359fb269e0";
 
-// export const BASE_URL = "https://pixabay.com/api/";
-// export const API_KEY = "46106680-1f6bf9cc8c5ebc2359fb269e0";
-// const ul = document.querySelector(".wrapper");
-// const urlParams = new URLSearchParams({ q: "", image_type: "photo", orientation: "horizontal", safesearch: "true" })
+export function fetchData(searchQuery) {
+const urlParams = new URLSearchParams({ q: searchQuery, image_type: "photo", orientation: "horizontal", safesearch: "true" });
 
-// export function fetchData(searchQuery) {
-//     return fetch(`${BASE_URL}?key=${API_KEY}&${urlParams}`).then(response => {
-//         if (!response.ok) {
-//             throw new Error('Sorry, there are no images matching your search query. Please try again!');
-//         }
-//         return response.json();
-//     })
-// }
-// const form = document.querySelector(".container");
-
-// function checkForm(event) {
-//     event.preventDefault();
-//     const form = event.currentTarget;
-//     const { query } = form.elements;
-//     fetchData(query.value).then(data => {
-//         const imagesMarkup = renderMarkup(data);
-//      ul.insertAdjacentHTML("beforeend", dataMarkup);
-   
-   
-// }).catch(error => {
-//    iziToast.error({
-//     title: 'Error',
-//     message: error.message,
-// });
-// });
-  
-//     function renderMarkup({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) {
-//         return `<li class="image">
-//         <img src = "${webformatURL}" alt = "${tags}" />
-//         <div class = "card-body">   
-//           <p class="text">Likes ${likes}</p>
-//           <p class="text">Views ${views}</p>
-//           <p class="text">Comments ${comments}</p>
-//           <p class="text">Downloads ${downloads}</p>
-//           </div>
-//         </li>`;
-
-//     }
-    
-    
-//     // if (inputEl.trim() === "") {
-//     //     btn.disabled = true;
-//     //     return;
-//     // }
-//     // else {
-//     //     btn.disabled = false;
-//     // }
-// }
-
-// form.addEventListener("submit", checkForm);
-
-// // fetchData(query.value).then(data => {
-// //     const { hits } = data;
-// //     const dataMarkup = hits.reduce((acc, item) => {
-// //         return (acc += `<li class="image">
-// //         <img src = "${item.webformatURL}" alt = "${item.tags}" />
-// //         <div class = "card-body">   
-// //           <p class="text">Likes ${item.likes}</p>
-// //           <p class="text">Views ${item.views}</p>
-// //           <p class="text">Comments ${item.comments}</p>
-// //           <p class="text">Downloads ${item.downloads}</p>
-// //           </div>
-// //         </li>`);
-
-// //     }, "");
-        
-// //     ul.insertAdjacentHTML("beforeend", dataMarkup);
-   
-// // }).catch(error => {
-// //     console.error(error.message);
-// // });
+return fetch(`${BASE_URL}?key=${API_KEY}&${urlParams}`).then(response => {
+        if (!response.ok) {
+            throw new Error('Sorry, there are no images matching your search query. Please try again!');
+        }
+        return response.json();
+    })
+};
--- src/main.js
+++ src/main.js
@@ -3,45 +3,20 @@ import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import { BASE_URL, API_KEY, fetchData } from "./js/pixabay-api";
+import { showErrorMessage, loader, toggleLoader, renderMarkup, lightbox } from "./js/render-functions";
+
 document.head.insertAdjacentHTML("beforeend", `<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital@0;1&display=swap" rel="stylesheet"><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;1,500&display=swap" rel="stylesheet">`)
 
-export const BASE_URL = "https://pixabay.com/api/";
-export const API_KEY = "46106680-1f6bf9cc8c5ebc2359fb269e0";
-
 const ul = document.querySelector(".gallery");
 const btn = document.querySelector(".btn");
 const form = document.querySelector(".container");
-const loader = document.querySelector(".div-loader");
-
-
-export function fetchData(searchQuery) {
-const urlParams = new URLSearchParams({ q: searchQuery, image_type: "photo", orientation: "horizontal", safesearch: "true" });
-
-return fetch(`${BASE_URL}?key=${API_KEY}&${urlParams}`).then(response => {
-        if (!response.ok) {
-            throw new Error('Sorry, there are no images matching your search query. Please try again!');
-        }
-        return response.json();
-    })
-}
-
-
-function showErrorMessage(message) {
-    iziToast.error({
-        title: 'Error',
-        message,
-    });
-}
-
-function toggleLoader(show) {
-    loader.style.visibility = show ? "visible" : "hidden";
-}
 
 function checkForm(event) {
+
     event.preventDefault();
-    
     const form = event.currentTarget;
     const searchQuery = form.elements.query.value.trim();
 
@@ -54,11 +29,11 @@ function checkForm(event) {
     toggleLoader(true);
     btn.disabled = true;
   
-
     fetchData(searchQuery).then(data => {
+
        if (data.hits.length === 0) {
            showErrorMessage('Sorry, there are no images matching your search query. Please try again!');
-            return;
+           return;
         }
  
         const images = data.hits;
@@ -66,8 +41,8 @@ function checkForm(event) {
         ul.insertAdjacentHTML("beforeend", imagesMarkup);
         lightbox.refresh();
         form.reset();
-    })
         
+    })
         .catch(error => {
         showErrorMessage(error.message);
         })
@@ -76,27 +51,6 @@ function checkForm(event) {
         toggleLoader(false)
         btn.disabled = false;
     });
-  
-    function renderMarkup({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) {
-        return `<li class="image">
-        <a class = "gallery-link" href = "${webformatURL}">  
-        <img src = "${largeImageURL}" alt = "${tags}" />
-        </a>
-        <div class = "card-body">   
-          <p class="text">Likes <span class = "value">${likes}</span></p>
-          <p class="text">Views <span class = "value">${views}</span></p>
-          <p class="text">Comments <span class = "value">${comments}</span></p>
-          <p class="text">Downloads <span class = "value">${downloads}</span></p>
-          </div>
-        </li>`;
- 
-    }
 }
-
-const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',        
-    captionPosition: 'bottom',  
-    captionDelay: 250, 
-});
 
 form.addEventListener("submit", checkForm);
